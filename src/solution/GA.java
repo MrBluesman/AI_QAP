89842b81a417f9b197a7c21f178d0080fc24d721
Łukasz Parysek <l.parysek@gmail.com>
2018-02-26 13:38:40 +0100

    GA + Test classes

---
+++ src/solution/GA.java
@@ -0,0 +1,19 @@
+package solution;
+
+public class GA
+{
+    private static short N;
+    private static short[][] distances; //BetweenLocations
+    private static short[][] flows; //BetweenFacilities
+
+    public GA()
+    {
+
+    }
+
+    public GA(String _fileName)
+    {
+
+    }
+    
+}
---
+++ src/solution/Test.java
@@ -0,0 +1,9 @@
+package solution;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("START");
+    }
+}
